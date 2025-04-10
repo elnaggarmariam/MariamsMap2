0a9306b196bf622b35c49407cb36af25e6ad06ec
elnaggarmariam <mnaggar@sas.upenn.edu>
2025-04-10 14:55:16 -0400

    Add files via upload

---
+++ js/setup.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+  center: [30.0420, 30.9474],
+  zoom: 13,
+});
+
+var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
